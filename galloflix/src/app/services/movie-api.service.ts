7cc60a5ee0e941027b10b18cdf42d94c4c39b49a
miguelpiras <miguelpiras08@gmail.com>
2025-05-19 22:32:31 -0300

    create: carrossel com os dados da api

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -9,5 +9,18 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
-  
+  baseUrl = 'https://api.themoviedb.org/3';
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTMxYmU1ZmM0YTUzMGQxOGMxNDllYmZmNDZkMzY1MyIsIm5iZiI6MTc0NzA5NTAxOC4zNjgsInN1YiI6IjY4MjI4ZGVhNzUyYWJiYmJmODVhMmU4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oEzSiJlegIVs7hxea5-TLv7kKjzHFr0jyGIxNlOuWxA'
+    }
+  };
+
+  // Dados para o Banner - Midias em destaque da semana
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
+
 }
